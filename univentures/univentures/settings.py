a98b1412b617339ce262df0fda3ec4ed998d82a4
Young Eun Kim <aakim0907@gmail.com>
2017-08-10 13:44:17 -0700

    update rest api model

--- univentures/univentures/settings.py
+++ univentures/univentures/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'companies.app.CompaniesConfig'
+    'companies.apps.CompaniesConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
